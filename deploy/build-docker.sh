9acbe743b780e7b40a729798f5e05506a00f12c2
Sopark0724 <sopark0724@gmail.com>
2021-04-20 08:21:24 +0900

    Update build-docker.sh

--- deploy/build-docker.sh
+++ deploy/build-docker.sh
@@ -4,6 +4,6 @@ PROJECT_ID=$1
 VERSION=$2
 mkdir -p $BUILD_PATH && (cd $BUILD_PATH; jar -xf ../libs/*.jar)
 
-docker build -t gcr.io/$PROJECT_ID/hello-app:$VERSION .
+docker build --build-arg DEPENDENCY=$BUILD_PATH -t gcr.io/$PROJECT_ID/hello-app:$VERSION .
 
 docker push gcr.io/$PROJECT_ID/hello-app:$VERSION
